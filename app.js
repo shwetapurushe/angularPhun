71fbbc7d2d1ea133ec1031dbe59a36087707d11d
shwetapurushe <shwetapurushe@gmail.com>
2015-08-25 14:57:40 -0400

    succeeded with expandable tree in ui grid (web example)

--- app.js
+++ app.js
@@ -4,5 +4,10 @@
 
 (function(){
 
-    angular.module('phun_app', ['phun_app.myModule', 'ui.grid', 'ui.grid.treeView']);
+    angular.module('phun_app', ['phun_app.myModule',
+                                'ui.grid',
+                                'ui.grid.treeView',
+                                'ui.grid.expandable',
+                                'ui.grid.pinning',
+                                'ui.grid.selection']);
 })();
--- src/myModule/testController.js
+++ src/myModule/testController.js
@@ -13,49 +13,81 @@
         tCtrl.testService = testService;
 
         tCtrl.gridOptions = {
-            enableSorting: true,
-            enableFiltering: true,
-            showTreeExpandNoChildren: true,
-            columnDefs: [
-                { name: 'name', width: '30%' },
-                { name: 'gender', width: '20%' },
-                { name: 'age', width: '20%' },
-                { name: 'company', width: '25%' },
-                { name: 'state', width: '35%' },
-                { name: 'balance', width: '25%', cellFilter: 'currency' }
-            ],
-            onRegisterApi: function( gridApi ) {
-                $scope.gridApi = gridApi;
-                $scope.gridApi.treeBase.on.rowExpanded($scope, function(row) {
-                    if( row.entity.$$hashKey === $scope.gridOptions.data[50].$$hashKey && !$scope.nodeLoaded ) {
-                        $interval(function() {
-                            $scope.gridOptions.data.splice(51,0,
-                                {name: 'Dynamic 1', gender: 'female', age: 53, company: 'Griddable grids', balance: 38000, $$treeLevel: 1},
-                                {name: 'Dynamic 2', gender: 'male', age: 18, company: 'Griddable grids', balance: 29000, $$treeLevel: 1}
-                            );
-                            $scope.nodeLoaded = true;
-                        }, 2000, 1);
+          expandableRowTemplate : 'src/myModule/subGrid.html',
+          expandableRowHeight : 150,
+          expandableRowScope : {
+              subGridVariable : 'subGridScopeVariable'
+          }
+        };
+
+        tCtrl.gridOptions.columnDefs = [
+            { name: 'id' },
+            { name: 'name'},
+            { name: 'age'},
+            { name: 'address.city'}
+        ];
+
+        $http.get('lib/500_complex.json')
+            .success(function(data){
+                for(var i =0 ;i < data.length; i++){
+                    data[i].subGridOptions = {
+                        columnDefs : [{name : "Id", field : "id"}, {name: "Name", field : "name"}],
+                        data : data[i].friends
                     }
-                });
-            }
+                }
+                tCtrl.gridOptions.data = data;
+        });
+
+        tCtrl.gridOptions.onRegisterApi = function (gridApi){
+            tCtrl.gridApi = gridApi;
         };
 
 
-        $http.get('lib/500_complex.json')
-            .success(function(data) {
-                for ( var i = 0; i < data.length; i++ ){
-                    data[i].state = data[i].address.state;
-                    data[i].balance = Number( data[i].balance.slice(1).replace(/,/,'') );
-                }
-                data[0].$$treeLevel = 0;
-                data[1].$$treeLevel = 1;
-                data[10].$$treeLevel = 1;
-                data[11].$$treeLevel = 1;
-                data[20].$$treeLevel = 0;
-                data[25].$$treeLevel = 1;
-                data[50].$$treeLevel = 0;
-                data[51].$$treeLevel = 0;
-                tCtrl.gridOptions.data = data;
-            });
+        //works for tree view
+        //tCtrl.gridOptions = {
+        //    enableSorting: true,
+        //    enableFiltering: true,
+        //    showTreeExpandNoChildren: true,
+        //    columnDefs: [
+        //        { name: 'name', width: '30%' },
+        //        { name: 'gender', width: '20%' },
+        //        { name: 'age', width: '20%' },
+        //        { name: 'company', width: '25%' },
+        //        { name: 'state', width: '35%' },
+        //        { name: 'balance', width: '25%', cellFilter: 'currency' }
+        //    ],
+        //    onRegisterApi: function( gridApi ) {
+        //        $scope.gridApi = gridApi;
+        //        $scope.gridApi.treeBase.on.rowExpanded($scope, function(row) {
+        //            if( row.entity.$$hashKey === $scope.gridOptions.data[50].$$hashKey && !$scope.nodeLoaded ) {
+        //                $interval(function() {
+        //                    $scope.gridOptions.data.splice(51,0,
+        //                        {name: 'Dynamic 1', gender: 'female', age: 53, company: 'Griddable grids', balance: 38000, $$treeLevel: 1},
+        //                        {name: 'Dynamic 2', gender: 'male', age: 18, company: 'Griddable grids', balance: 29000, $$treeLevel: 1}
+        //                    );
+        //                    $scope.nodeLoaded = true;
+        //                }, 2000, 1);
+        //            }
+        //        });
+        //    }
+        //};
+        //
+        //
+        //$http.get('lib/500_complex.json')
+        //    .success(function(data) {
+        //        for ( var i = 0; i < data.length; i++ ){
+        //            data[i].state = data[i].address.state;
+        //            data[i].balance = Number( data[i].balance.slice(1).replace(/,/,'') );
+        //        }
+        //        data[0].$$treeLevel = 0;
+        //        data[1].$$treeLevel = 1;
+        //        data[10].$$treeLevel = 1;
+        //        data[11].$$treeLevel = 1;
+        //        data[20].$$treeLevel = 0;
+        //        data[25].$$treeLevel = 1;
+        //        data[50].$$treeLevel = 0;
+        //        data[51].$$treeLevel = 0;
+        //        tCtrl.gridOptions.data = data;
+        //    });
     }
 })();
