b3c7eb3278e776099197a32e207e882d6d554266
shwetapurushe <shwetapurushe@gmail.com>
2015-09-03 14:23:07 -0400

    playing with angular tree

--- app.js
+++ app.js
@@ -4,10 +4,13 @@
 
 (function(){
 
-    angular.module('phun_app', ['phun_app.myModule',
-                                'ui.grid',
-                                'ui.grid.treeView',
-                                'ui.grid.expandable',
-                                'ui.grid.pinning',
-                                'ui.grid.selection']);
+    //angular.module('phun_app', ['phun_app.myModule',
+    //                            'phun_app.myTree',
+    //                            'ui.grid',
+    //                            'ui.grid.treeView',
+    //                            'ui.grid.expandable',
+    //                            'ui.grid.pinning',
+    //                            'ui.grid.selection']);
+
+    angular.module('phun_app', ['phun_app.myTree', 'ui.tree']);
 })();
