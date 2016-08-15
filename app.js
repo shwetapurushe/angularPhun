28280129e4c7674e2db32c2e5d90679f5bb2e68d
shwetapurushe <shwetapurushe@gmail.com>
2016-08-15 16:53:30 -0400

    recap one bootstrapping and basic expression

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
-/**
- * Created by Shweta on 5/10/15.
- */
-
 (function(){
+	"use strict";
+	angular.module('myApp', []);
 
-    //angular.module('phun_app', ['phun_app.myModule',
-    //                            'phun_app.myTree',
-    //                            'ui.grid',
-    //                            'ui.grid.treeView',
-    //                            'ui.grid.expandable',
-    //                            'ui.grid.pinning',
-    //                            'ui.grid.selection']);
+	angular.module('myApp').controller('mainController', mainController);
 
-    angular.module('phun_app', ['phun_app.myTree', 'ui.tree']);
+	function mainController ()
+	{
+		var mainCtrl = this;
+
+		mainCtrl.greeting = "Hello World!"
+	}
+
 })();
